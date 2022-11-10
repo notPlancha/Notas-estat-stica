2165d281bda84b1d66c323971f2aecbf7b7278f3
notPlancha <andre.plancha@hotmail.com>
2022-11-10 12:05:22 +0000

    lowered to 6 seconds compile

--- render.ps1
+++ render.ps1
@@ -1,13 +1,10 @@
-$error.clear()
-try {
-    R.exe -e "knitr::knit(here::here('notasEstatistica.rmd'))"
-    pandoc ".\notasEstatistica.md" -o ".\notasEstatistica.tex" --standalone
-    .\tectonic.exe .\notasEstatistica.tex
-}
-catch {
-    
-}
-if(!$error){
-    #Remove-Item .\notasEstatistica.tex
-    #Remove-Item .\notasEstatistica.md
-}
+R.exe -e "knitr::knit(here::here('notasEstatistica.rmd'))"
+if($LASTEXITCODE) {return}
+pandoc ".\notasEstatistica.md" -o ".\notasEstatistica.tex" --standalone
+#.\umdoc.exe .\notasEstatistica.md -o .\notasEstatistica.tex
+if($LASTEXITCODE) {return}
+.\tectonic.exe .\notasEstatistica.tex
+if($LASTEXITCODE) {return}
+Remove-Item .\notasEstatistica.tex
+Remove-Item .\notasEstatistica.md
+

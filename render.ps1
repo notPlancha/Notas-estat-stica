6960b5fd28066613f89087190415d6549ed96686
notPlancha <andre.plancha@hotmail.com>
2022-11-11 14:09:44 +0000

    changed to rtex

--- render.ps1
+++ render.ps1
@@ -1,10 +1,5 @@
-R.exe -e "knitr::knit(here::here('notasEstatistica.rmd'))"
-if($LASTEXITCODE) {return}
-pandoc ".\notasEstatistica.md" -o ".\notasEstatistica.tex" --standalone
-#.\umdoc.exe .\notasEstatistica.md -o .\notasEstatistica.tex
+R.exe -e "knitr::knit(here::here('notasEstatistica.rtex'))"
 if($LASTEXITCODE) {return}
 .\tectonic.exe .\notasEstatistica.tex
 if($LASTEXITCODE) {return}
 Remove-Item .\notasEstatistica.tex
-Remove-Item .\notasEstatistica.md
-
